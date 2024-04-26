7a8219a11c40843b045d3b43823d51cb2857fd7d
Fr34k <cedric.steiger@gmail.com>
2024-04-26 23:08:36 +0000

    Use tailwindcss & vite

--- resources/views/livewire/result-list.blade.php
+++ resources/views/livewire/result-list.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-xl:gap-4 gap-6">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-xl:gap-3 gap-6">
     @forelse ($deals as $deal)
         <livewire:results-card wire:key="{{ $deal->id }}" :deal="$deal" />
     @empty
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,14 +11,40 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 
 <body class="font-[sans-serif] antialiased bg-gray-100">
     <div class="p-4 mx-auto lg:max-w-7xl sm:max-w-full">
-        <h2 class="text-4xl font-extrabold text-gray-800 mb-2">Deals</h2>
+        <h2 class="text-4xl font-extrabold text-gray-800 mb-3">Deals</h2>
         <livewire:result-filters />
         <livewire:result-list />
+
+
+        <footer class="mt-3 bg-white rounded-lg dark:bg-gray-800">
+            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    Made with ♥ by
+                    <a href="https://csteiger.ch" class="hover:underline">csteiger.ch</a>
+                </span>
+                <ul
+                    class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+
     </div>
 </body>
 
