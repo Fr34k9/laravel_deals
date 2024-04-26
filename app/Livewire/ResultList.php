dfdf5eed92b551e78802c27b5bb26c2e7a70de39
Fr34k <cedric.steiger@gmail.com>
2024-04-26 21:50:48 +0000

    Show different deals on local development

--- app/Livewire/ResultList.php
+++ app/Livewire/ResultList.php
@@ -10,9 +10,16 @@ class ResultList extends Component
     public function render()
     {
         $deals = Deal::orderBy('created_at', 'desc')
-            ->where('updated_at', '>=', now()->subMinutes(5))
-            ->where('products_left', '>', 0)
-            ->get();
+            ->where('products_left', '>', 0);
+
+        // if is local development
+        if (config('app.env') === 'local') {
+            $deals = $deals->where('updated_at', '>=', now()->subHours(12));
+        } else {
+            $deals = $deals->where('updated_at', '>=', now()->subMinutes(5));
+        }
+
+        $deals = $deals->get();
 
         return view('livewire.result-list', compact('deals'));
     }
