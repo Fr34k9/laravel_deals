5dc9deba191e39bb77fcd04c6c38838b7668b550
Fr34k <cedric.steiger@gmail.com>
2024-04-27 22:51:40 +0000

    Create command for job

---
+++ app/Console/Commands/Crawl.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\CrawlJob;
+use App\Models\Platform;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class Crawl extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crawl:start {platform?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Starts all crawlers';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $platform = $this->argument('platform');
+        if ($platform) {
+            if (is_numeric($platform)) {
+                $this->startCrawlerById($platform);
+            } else {
+                $this->startCrawlerByName($platform);
+            }
+        } else {
+            $this->startAllCrawlers();
+        }
+    }
+
+    private function startCrawlerById($id)
+    {
+        $platform = Platform::find($id);
+        if (!$platform) {
+            Log::error('Invalid ID');
+            return;
+        }
+
+        CrawlJob::dispatch($platform);
+    }
+
+    private function startCrawlerByName($name)
+    {
+        $platform = Platform::where('name', $name)->first();
+        if (!$platform) {
+            Log::error('Invalid name');
+            return;
+        }
+        CrawlJob::dispatch($platform);
+    }
+
+    private function startAllCrawlers()
+    {
+        $platforms = Platform::all();
+        foreach($platforms as $platform) {
+            CrawlJob::dispatch($platform);
+        }
+    }
+}
