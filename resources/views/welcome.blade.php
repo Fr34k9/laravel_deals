b55315daf290b792c72579421233d62b7bd6a922
Fr34k <cedric.steiger@gmail.com>
2024-04-27 11:56:27 +0000

    Styling frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,24 +5,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <title>Deals</title>
 
     @vite('resources/css/app.css')
 </head>
 
 <body class="font-[sans-serif] antialiased bg-gray-100">
     <div class="p-4 mx-auto lg:max-w-7xl sm:max-w-full">
-        <h2 class="text-4xl font-extrabold text-gray-800 mb-3">Deals</h2>
+        <h2 class="mb-3 text-4xl font-extrabold text-gray-800">
+            <a href="{{ route('home') }}" wire:navigate>Deals</a>
+        </h2>
         <livewire:result-filters />
         <livewire:result-list />
 
-
         <footer class="mt-3 bg-white rounded-lg dark:bg-gray-800">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <div class="w-full max-w-screen-xl p-4 mx-auto md:flex md:items-center md:justify-between">
                 <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                     Made with ♥ by
                     <a href="https://csteiger.ch" class="hover:underline">csteiger.ch</a>
@@ -44,7 +41,6 @@
                 </ul>
             </div>
         </footer>
-
     </div>
 </body>
 
