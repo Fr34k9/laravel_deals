fd8eef3da1c6cd7c4893575fede3f7cb82b0cfbe
Fr34k <cedric.steiger@gmail.com>
2024-04-28 20:41:07 +0000

    Remove spatie/crawler as dependency

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('crawl/{crawler}', function ($crawler) {
+    if( is_numeric($crawler) ) {
+        $platform = Platform::find($crawler);
+        if (!$platform) {
+            dd('Invalid ID');
+        }
+        $crawler = $platform->name;
+    }
+
+    $crawler = 'App\Crawlers\\' . ucfirst($crawler);
+    if(!class_exists($crawler)) {
+        dd('Crawler not found');
+    }
+    new $crawler();
+});
