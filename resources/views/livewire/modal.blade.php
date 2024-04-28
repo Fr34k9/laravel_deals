421a5cbafaff9bce1d77f1ec01734ce52b156d5a
Fr34k <cedric.steiger@gmail.com>
2024-04-28 19:10:52 +0000

    Change style of modal

--- resources/views/livewire/modal.blade.php
+++ resources/views/livewire/modal.blade.php
@@ -1,36 +1,36 @@
-<div x-init class="bg-red-500" x-data="{ isOpen: @entangle('isOpen') }" @keydown.esc="isOpen = false">
-    <div x-show="isOpen" x-cloak
-        class="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto">
-        <div class="relative w-full h-full max-w-xl p-4 md:h-auto">
-            <!-- Modal content -->
-            <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-                <!-- Modal header -->
-                <div class="absolute top-3 right-3">
-                    <button type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex dark:hover:bg-gray-600 dark:hover:text-white"
-                        @click="isOpen = false">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                    </button>
-                </div>
-                <span>
-                    Aktuell gibt es Crawler der folgenden Plattformen: <br><br>
-                    <ul class="">
-                        @foreach ($platforms as $platform)
-                            <li>
-                                <a href="{{ $platform->url }}" class="hover:underline">
-                                    <img class="inline w-4 h-4 m-1" src="{{ $platform->image }}" /> {{ $platform->name }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </span>
-            </div>
+<div class="text-white" x-data="{ isOpen: @entangle('isOpen') }" x-show="isOpen">
+    <div
+        class="translate-x-0 fixed left-0 top-0 inset-x-0 transition-all duration-300 transform max-w-sm size-full z-[80] bg-gray-500 border-b dark:bg-neutral-800 dark:border-neutral-700"
+        tabindex="-1">
+        <div class="flex items-center justify-between px-4 py-3 border-b dark:border-neutral-700">
+            <h3 class="font-bold dark:text-white">
+                Aktuelle Plattformen
+            </h3>
+            <button type="button" @click="isOpen = false"
+                class="flex items-center justify-center text-sm font-semibold text-gray-800 border border-transparent rounded-full size-7 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700">
+                <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path d="M18 6 6 18"></path>
+                    <path d="m6 6 12 12"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="p-4">
+            <p class="text-gray-800 dark:text-neutral-400">
+                <ul class="">
+                    @foreach ($platforms as $platform)
+                        <li>
+                            <a href="{{ $platform->url }}" class="hover:underline">
+                                <img class="inline w-4 h-4 m-1" src="{{ $platform->image }}" /> {{ $platform->name }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </p>
+            <p class="mt-3">
+                <a class="mt-3" href="github" target="_blank">More details here..</a>
+            </p>
         </div>
     </div>
-    <div x-show="isOpen" x-cloak class="fixed inset-0 z-40 bg-black opacity-25"></div>
 </div>
