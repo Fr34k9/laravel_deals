17def2ce75d916f3b5c643b873dfd72744cd5e5d
Fr34k <cedric.steiger@gmail.com>
2024-05-10 21:11:57 +0000

    Crawling by url only allowed for admin

--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,22 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::get('crawl/{crawler}', function ($crawler) {
-    if( is_numeric($crawler) ) {
-        $platform = Platform::find($crawler);
-        if (!$platform) {
-            dd('Invalid ID');
+    Route::get('crawl/{crawler}', function ($crawler) {
+        if( is_numeric($crawler) ) {
+            $platform = Platform::find($crawler);
+            if (!$platform) {
+                dd('Invalid ID');
+            }
+            $crawler = $platform->name;
         }
-        $crawler = $platform->name;
-    }
 
-    $crawler = 'App\Crawlers\\' . ucfirst($crawler);
-    if(!class_exists($crawler)) {
-        dd('Crawler not found');
-    }
-    new $crawler();
+        $crawler = 'App\Crawlers\\' . ucfirst($crawler);
+        if(!class_exists($crawler)) {
+            dd('Crawler not found');
+        }
+        new $crawler();
+    });
 });
 
 require __DIR__.'/auth.php';
