f81b1fea7291bd5ff89daa45abb4daf95fb3c46b
Fr34k <cedric.steiger@gmail.com>
2024-09-08 00:11:51 +0200

    Better analyzing, add last time crawled

--- resources/views/livewire/result-list.blade.php
+++ resources/views/livewire/result-list.blade.php
@@ -1,14 +1,21 @@
-<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 max-xl:gap-3">
-    @forelse ($deals as $deal)
-        <livewire:results-card wire:key="{{ $deal->id }}" :deal="$deal" />
-    @empty
-        @if( !empty($this->platform) )
-            <div class="text-lg text-gray-800 dark:text-white">No deals found for {{ $this->platform->name }}</div>
-        @else
-            <div class="text-gray-800 dark:text-white">
-                <div class="text-lg">No deals found.</div>
-                <div class="text-sm">This might be an error, please check the platforms by yourself.</div>
-            </div>
-        @endif
-    @endforelse
+<div>
+    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 max-xl:gap-3">
+        @forelse ($deals as $deal)
+            <livewire:results-card wire:key="{{ $deal->id }}" :deal="$deal" />
+        @empty
+            @if( !empty($this->platform) )
+                <div class="text-lg text-gray-800 dark:text-white">No deals found for {{ $this->platform->name }}</div>
+            @else
+                <div class="text-gray-800 dark:text-white">
+                    <div class="text-lg">No deals found.</div>
+                </div>
+            @endif
+        @endforelse
+    </div>
+    
+    @if( !empty($this->platform) )
+        <div class="mt-3 text-sm text-center text-gray-500 dark:text-gray-400">
+            Last refresh for {{ $this->platform->name }}: {{ $this->platform->last_crawled->diffForHumans() }}
+        </div>
+    @endif
 </div>
